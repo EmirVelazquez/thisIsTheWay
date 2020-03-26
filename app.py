9f048f04acd117da9d5bf73a0a583188a0897085
EmirVelazquez <emirleovelazquez@gmail.com>
2020-03-25 23:14:56 -0500

    started working with different data types

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
+print(4 + 4)
+
+hero_name = "Bruno"
+outside_times = 4
+eat_times = 2.0
+is_cat = False
+
+print("There once was a dog named " + hero_name + ", ")
+print("he loved going outside about " + outside_times + " times a day.")
+print(hero_name + " also loved to eat all day.")
+print("He ate about " + eat_times + " a day to fuel his energy.")
