2a185d3bc92aa719bdbae164b730b77b1b734443
EmirVelazquez <emirleovelazquez@gmail.com>
2020-03-26 22:25:00 -0500

    played around with some of the most used string built in functions

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
-print(4 + 4)
-
-hero_name = "Bruno"
-outside_times = 4
-eat_times = 2.0
-is_cat = False
-
-print("There once was a dog named " + hero_name + ", ")
-print("he loved going outside about " + outside_times + " times a day.")
-print(hero_name + " also loved to eat all day.")
-print("He ate about " + eat_times + " a day to fuel his energy.")
+# Common examples of working with string functions
+phrase = "Peky's Bakery"
+print(phrase.lower())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("e"))
+print(phrase.replace("Peky's", "Bruno & Peky's"))
