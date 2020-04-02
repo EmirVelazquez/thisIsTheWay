394a30d5a3fdb95d9df6e0d486e1919be035487a
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-01 21:29:35 -0500

    created a tuples.py file, learned about tuples

---
+++ tuples.py
@@ -0,0 +1,4 @@
+# Tuples are immutable (an immutable object is an object whose state cannot be modified after it is created.)
+# Good for data that is never going to change
+coordinates = (32.04950, 91.20394)
+print(coordinates[1])
