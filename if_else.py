4f562c8ee14ef590322fdb44763aeb6e2f38ebbc
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-03 20:14:59 -0500

    made a comparin function, similar to js

--- if_else.py
+++ if_else.py
@@ -1,6 +1,6 @@
 # Conditionals
-is_male = False
-is_tall = False
+is_male = True
+is_tall = True
 
 if is_male and is_tall:
     print("You are a tall male")
@@ -10,3 +10,15 @@ elif not (is_male) and is_tall:
     print("You are not a male but are tall")
 else:
     print("You are not a male and are not tall")
+
+# Comparisons
+def max_number(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_number(35, 51, 9))
