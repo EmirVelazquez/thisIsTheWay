24fa23b30b41c7bec6bcfda9e22b3d9eb791f1b6
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-06 22:21:30 -0500

    created a forLoop file, learned about and tested some for loops

---
+++ forLoop.py
@@ -0,0 +1,19 @@
+# Looping through a string
+for letter in "Michael Scott":
+    print(letter)
+
+# Looping through a list
+friends = ["Jim", "Pam", "Dwight"]
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for friend in friends:
+    print(friend)
+
+# Looping using a range()
+for index in range(10):
+    print(index)
+
+for index in range(3, 10):
+    print(index)
