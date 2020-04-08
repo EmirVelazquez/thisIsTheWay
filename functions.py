34900723efb86500166145b8611efbf97b9ef71a
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-07 22:02:34 -0500

    worked with making an exponential function, called it with parameters

--- functions.py
+++ functions.py
@@ -13,3 +13,15 @@ def cube_this(numb):
 
 result = cube_this(3)
 print(result)
+
+# Exponent function
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+
+print(raise_to_power(3, 4))
+print(raise_to_power(3, 2))
+print(raise_to_power(2, 3))
