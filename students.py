c52abe3af5827079510ab6489579db1e9c94e5d3
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-10 22:23:01 -0500

    renamed class files, added a function inside of student class, used function to check if students are on the dean's list

--- students.py
+++ students.py
@@ -1,9 +1,16 @@
 # Importing the Student class from the studentFile that has the class structure
-from studentFile import Student
+from studentClass import Student
 
 # Making an object here using the Student class
 student1 = Student("Ricky Bobby", "Culinary Arts", 3.5, False)
 student2 = Student("Michael Scott", "Business Management", 1.9, True)
+student3 = Student("Bruno Velazquez", "Psychology", 4.0, True)
 
+# Printing object values from student class
 print(student1.name, student1.gpa)
 print(student2.name, student1.major)
+
+# Using function in class to check if the student is on dean's list
+print(student3.on_dean_list())
+print(student2.on_dean_list())
+print(student1.on_dean_list())
